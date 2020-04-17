12bae3b77c8ffca0df9b4d0c4804207492c70b76
yangyihan16@mails.ucas.ac.cn <yangyihan16@mails.ucas.ac.cn>
2020-04-17 11:26:43 +0800

    Update api.c

--- mtcp/src/api.c
+++ mtcp/src/api.c
@@ -1891,7 +1891,7 @@ WriteProcess(mctx_t mctx, int sockid, size_t len){
 
 int 
 SendProcess(mctx_t mctx, int sockid, int recv_len, int send_len){
-	TRACE_API(" === SendProcess ===\n");
+	printf(" === SendProcess ===\n");
 
 	mtcp_manager_t mtcp;
 	socket_map_t socket;
@@ -1944,7 +1944,7 @@ SendProcess(mctx_t mctx, int sockid, int recv_len, int send_len){
 		SQ_LOCK(&mtcp->ctx->sendq_lock);
 		sndvar->on_sendq = TRUE;
 		StreamEnqueue(mtcp->sendq, cur_stream);		/* this always success */
-		TRACE_API(" >> Enqueued event to send queue\n");
+		printf(" >> Enqueued event to send queue\n");
 		SQ_UNLOCK(&mtcp->ctx->sendq_lock);
 		mtcp->wakeup_flag = TRUE;
 	}
