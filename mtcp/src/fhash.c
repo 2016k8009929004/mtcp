73b17836008ac5e5dd803005a1b69e4270f03619
yihan <yangyihan16@mails.ucas.ac.cn>
2021-01-30 20:48:31 +0800

    update

--- mtcp/src/fhash.c
+++ mtcp/src/fhash.c
@@ -83,9 +83,9 @@ StreamHTInsert(struct hashtable *ht, void *it)
 	assert(ht);
 
 	idx = ht->hashfn(item);
-	printf(" [%s] hash index: %d\n", __func__, idx);
 	assert(idx >=0 && idx < NUM_BINS_FLOWS);
 
+	printf(" [%s] HT addr: %p, index: %d, head: %p\n", __func__, ht, idx, &ht->ht_table[ht->hashfn(item)]);
 	TAILQ_INSERT_TAIL(&ht->ht_table[idx], item, rcvvar->he_link);
 
 	item->ht_idx = TCP_AR_CNT;
@@ -115,9 +115,9 @@ StreamHTSearch(struct hashtable *ht, const void *it)
 	hash_bucket_head *head;
 
 	idx = ht->hashfn(item);
-	printf(" [%s] hash index: %d\n", __func__, idx);
 
 	head = &ht->ht_table[ht->hashfn(item)];
+	printf(" [%s] HT addr: %p, index: %d, head: %p\n", __func__, ht, idx, head);
 	TAILQ_FOREACH(walk, head, rcvvar->he_link) {
 		if (ht->eqfn(walk, item)) 
 			return walk;
